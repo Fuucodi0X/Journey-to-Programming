3243ba4cbc9fbfb5f46196b83fa687267a23f0b0
Fuucodi0X <akmelf20@gmail.com>
2023-11-26 14:52:55 +0300

    Suu first to commit calculator

---
+++ calculator/cal.py
@@ -0,0 +1,28 @@
+# Simple Calculator
+
+# Ask the operation
+operator = input("Define an operator: ")
+# Get operands
+oprands = input("List as many operand as you want(separated by space daa!): ").split()
+# Convert form string to float
+nums = [float(oprand) for oprand in oprands]
+
+
+value = nums[0]
+for i in range(1,len(nums)):
+    if operator == '+':
+        value += nums[i]
+    elif operator == '-':
+        value -= nums[i]
+    elif operator == '*':
+        value *= nums[i]
+    elif operator == '**':
+        value **= nums[i]
+    elif operator == '/':
+        value /= nums[i]
+    else:
+        print("Invalid operator use + - * / **")
+    
+
+
+print(value)
